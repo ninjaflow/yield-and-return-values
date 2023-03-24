aef3c335c13eb18cb3507ad6eb0c99ae9c0bad3c
Lluvia <friasllu@gmail.com>
2023-03-24 13:12:22 -0400

    commit tree 89e2d7014736f90da8c1662f2d8840c71d90c8e6

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,14 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+   collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+#hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" } --the return value for this will be "nil" so delete "puts" to RETURN value of code 
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
